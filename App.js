6c04fe746451e524d162e305f50d467cb8e4510b
AhmedAboAbadi <ahmed.m.aboabadi@gmail.com>
2022-09-22 11:17:51 +0300

    init

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello, I'm Ahmed Moahmmed Abo Abadi</Text>
       <StatusBar style="auto" />
     </View>
   );
