9bb3a6139df9e537f9117c980a983d4c86bb9417
AhmedAboAbadi <ahmed.m.aboabadi@gmail.com>
2022-09-25 13:43:32 +0300

    add delete icon

--- components/ToDoItem.js
+++ components/ToDoItem.js
@@ -1,10 +1,14 @@
 import React from "react";
-import { StyleSheet, Text, TouchableOpacity } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
 
 export default function ToDoItem({ item, pressHandler }) {
   return (
     <TouchableOpacity onPress={() => pressHandler(item.key)}>
-      <Text style={styles.item}>{item.text} </Text>
+      <View style={styles.item}>
+        <MaterialIcons name="delete" size={18} />
+        <Text style={styles.text}>{item.text} </Text>
+      </View>
     </TouchableOpacity>
   );
 }
@@ -17,5 +21,10 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderStyle: "dashed",
     borderRadius: 10,
+    flexDirection: "row",
+  },
+  text: {
+    marginLeft: 10,
+    cursor: "pointer",
   },
 });
